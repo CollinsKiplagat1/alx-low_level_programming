daab5537f6a0370fce25e22bcf8830ead5ed1638
CollinsKiplagat1 <collinskipla1@gmail.com>
2022-06-06 17:19:09 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+#include <ctype.h>
+
+/**
+ * main - print the alphabet in lowercase,followed by a new line
+ * You can only use the putchar 2 times
+ * Return: 0
+ */
+
+int main(void)
+{
+int lw = 'a';
+while (lw <= 'z')
+{
+putchar(lw);
+lw += 1;
+{
+putchar('\n');
+return (0);
+}
