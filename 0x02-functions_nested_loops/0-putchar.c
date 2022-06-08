4ae0a6b15a3c6d54ea6ec806a0a6380f046a8071
CollinsKiplagat1 <collinskipla1@gmail.com>
2022-06-07 23:57:52 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <unistd.h.>
+
+/**
+ * 0_putchar - writes the character to c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1 .
+ * On error, -1 is returned and no errror is et approriately
+ */
+
+int 0_putchar(char c)
+{
+return (write(1, &c, 1));
+}
